0f19b2bc151f3f086b17a42805848e6ee64a5f0c
Ramneek Kalra <kalraramneekhmr@gmail.com>
2018-06-08 00:47:18 +0530

    GWT Application using Netbeans IDE.

--- src/java/com/ramneek/client/mainEntryPoint.java
+++ src/java/com/ramneek/client/mainEntryPoint.java
@@ -10,8 +10,12 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.ui.RootPanel;
-import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+
 
 /**
  * Main entry point.
@@ -32,15 +36,42 @@ public class mainEntryPoint implements EntryPoint {
      */
     public void onModuleLoad() {
         
-        final Button button = new Button("Click Me!");
+        final Button button = new Button("Submit Details");
+        final TextBox text = new TextBox();
+        final TextBox text1 = new TextBox();
+        
+        text.setFocus(true);
+        
+        final Label l = new Label("Enter your Name:");
+        final Label l1 = new Label("Enter your Class:");
+        final Label ld = new Label();
+        final Label ld1 = new Label();
+        
+        final DialogBox d = new DialogBox();
+        d.setAnimationEnabled(true);
+        
         
         button.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-                Window.alert("You are in GWT Application");
+                String name = text.getText();
+                String className = text1.getText();
+                ld.setText(name);
+                d.add(ld);
+                ld1.setText(className);
+                d.add(ld1);
+                d.show();
+                
             }
         });
+        
+        RootPanel.get().add(l);
+        RootPanel.get().add(text);
+        RootPanel.get().add(l1);
+        RootPanel.get().add(text1);
+        
         RootPanel.get().add(button);
+        
         
     }
 }
